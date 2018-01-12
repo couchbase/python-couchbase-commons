fa49c202cfb9a363eb3daa8cb995a3ed239d6265
Chris Hillery <ceej@couchbase.com>
2018-01-12 01:55:11 -0800

    Add N1QL-based query operation for CouchbaseBuild
    Change-Id: I92aa932fdae85c5478c73ad66bba0839180dbe63
    Reviewed-on: http://review.couchbase.org/87739
    Reviewed-by: Kenneth Lareau <kenneth.lareau@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- cbbuild/cbbuild/cbutil/db.py
+++ cbbuild/cbbuild/cbutil/db.py
@@ -5,7 +5,7 @@ the Python API
 
 import couchbase.bucket
 import couchbase.exceptions
-
+from couchbase.n1ql import N1QLQuery
 
 class NotFoundError(Exception):
     """Module-level exception for missing keys in database"""
@@ -17,11 +17,12 @@ class CouchbaseBuild:
     Represents a Build entry in the build database
     """
 
-    def __init__(self, db, product, version, bld_num):
-        """Loads build entry from database"""
+    def __init__(self, db, data):
+        """Constructor from dict"""
+
         self.__db = db
-        self.__key = f'{product}-{version}-{bld_num}'
-        self.__data = self.__db.get_document(self.__key)
+        self.__key = f'{data["key_"]}'
+        self.__data = data
 
     def __getattr__(self, key):
         """Passes through attribute requests to underlying data"""
@@ -36,6 +37,43 @@ class CouchbaseBuild:
 
         self.__data.setdefault('metadata', {})[key] = value
         self.__db.upsert_documents({self.__key: self.__data})
+
+    @property
+    def key(self):
+        return self.__key
+
+class CouchbaseCommit:
+    """
+    Represents a Commit entry in the build database
+    """
+
+    def __init__(self, db, data):
+        """Constructor from dict"""
+
+        self.__db = db
+        self.__key = f'{data["key_"]}'
+        self.__data = data
+
+    def __getattr__(self, key):
+        """Passes through attribute requests to underlying data"""
+
+        return self.__data.get(key)
+
+    @property
+    def key(self):
+        return self.__key
+
+    @property
+    def project(self):
+        """Computes the project name from the key"""
+
+        return '-'.join(self.__key.split('-')[0:-1])
+
+    @property
+    def sha(self):
+        """Computes the SHA from the key"""
+
+        return self.__key.split('-')[-1]
 
 class CouchbaseDB:
     """
@@ -55,7 +93,51 @@ class CouchbaseDB:
     def get_build(self, product, version, bld_num):
         """Get the CouchbaseBuild object for a specific build"""
 
-        return CouchbaseBuild(self, product, version, bld_num)
+        data = self.get_document(f'{product}-{version}-{bld_num}')
+        return CouchbaseBuild(self, data)
+
+    def query_builds(self, where_clause, **kwargs):
+        """
+        Uses a N1QL query to return a list of CouchbaseBuild objects.
+        Pass everything *after* the WHERE. You may also pass additional
+        named parameters which will be associated with $variables in
+        the query string.
+        """
+        query = N1QLQuery(
+            f"SELECT * FROM build_info WHERE type='build' AND {where_clause}",
+            **kwargs
+        )
+        return [
+            CouchbaseBuild(self, row["build_info"])
+            for row in self.bucket.n1ql_query(query)
+        ]
+
+    def get_commit(self, project, sha):
+        """Get the CouchbaseCommit object for a specific commit"""
+
+        return self.get_commit(f'{project}-{sha}')
+
+    def get_commit(self, commit_key):
+        """Get the CouchbaseCommit object for a specific commit, by key"""
+
+        data = self.get_document(commit_key)
+        return CouchbaseCommit(self, data)
+
+    def query_commits(self, where_clause, **kwargs):
+        """
+        Uses a N1QL query to return a list of CouchbaseCommit objects.
+        Pass everything *after* the WHERE. You may also pass additional
+        named parameters which will be associated with $variables in
+        the query string.
+        """
+        query = N1QLQuery(
+            f"SELECT * FROM build_info WHERE type='commit' AND {where_clause}",
+            **kwargs
+        )
+        return [
+            CouchbaseCommit(self, row["build_info"])
+            for row in self.bucket.n1ql_query(query)
+        ]
 
     def get_document(self, key):
         """Retrieve the document with the given key"""
